885148c234104f4ef56aa5900c8e645da9cd07fc
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:20:15 +0700

    Tuan 1/Bai 11/Xay dung phuong thuc dao nguoc mot chuoi

---
+++ BAI11/bai11/bai11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace bai11
+{
+    public class Program
+    {
+        public static string ChuoiDaoNguoc(string str)
+        {
+            char[] charArray = str.ToCharArray(); // Chuyển chuỗi thành mảng kí tự
+            Array.Reverse(charArray); // Hàm tự động đảo ngược mảng
+            return new string(charArray); // trả về mảng kí tự đã đảo ngược thành chuỗi
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap chuoi: ");
+            string str = Console.ReadLine();
+            str = ChuoiDaoNguoc(str); // Gọi phương thức để đảo ngược chuỗi
+            Console.WriteLine("Chuoi dao nguoc: " + str); //in ra chuỗi đảo ngược
+        }
+    }
+}
