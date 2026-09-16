de4b82fce0d638149a39d2aa4cb7fbdefed9ac8e
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:14:05 +0700

    Tuan 1/Bai 3/Lam chuong trinh nhap hai so nguyen x, y va tinh x^y

---
+++ BAI3/bai3/bai3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nhap y: ");
+            int y = int.Parse(Console.ReadLine());
+            double result = Math.Pow(x, y);
+            Console.WriteLine($"Ket qua {x} mu {y} la {result}");
+        }
+    }
+}
