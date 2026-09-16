da6837379dd2801145603f4d22f2cc32456aea39
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:17:32 +0700

    Tuan 1/Bai 7/Xay dung phuong thuc bool kiem tra so nguyen to

---
+++ BAI7/bai7/bai7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace bai7
+{
+    class Program
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen n: ");
+            int n = int.Parse(Console.ReadLine());
+            if (IsPrime(n))
+            {
+                Console.WriteLine($"{n} la so nguyen to.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} khong phai la so nguyen to.");
+            }
+        }
+    }
+}
