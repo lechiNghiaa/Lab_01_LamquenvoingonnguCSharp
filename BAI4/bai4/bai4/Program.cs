bec4ef35288b3d01473a395b033dbfc19f1dff4f
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:14:38 +0700

    Tuan 1/Bai 4/Lam lai bai 3 va xu ly loi khi x hoac y khong phai so nguyen

---
+++ BAI4/bai4/bai4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap x: ");
+            string x = Console.ReadLine();
+            if (!int.TryParse(x, out int numx))
+            {
+                Console.WriteLine("x khong phai la so nguyen!");
+                return;
+            }
+
+            Console.Write("Nhap y: ");
+            string y = Console.ReadLine();
+            if (!int.TryParse(y, out int numy))
+            {
+                Console.WriteLine("y khong phai la so nguyen!");
+                return;
+            }
+            double result = Math.Pow(numx, numy);
+            Console.WriteLine($"Ket qua {numx} mu {numy} la: {result}");
+        }
+    }
+}
