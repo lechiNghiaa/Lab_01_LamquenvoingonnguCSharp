41bb7dfbaca448131e4669ba06d99077d12d9bbd
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:19:09 +0700

    Tuan 1/Bai 9/Xay dung phuong thuc su dung out de tim gia tri lon nhat va nho nhat cua ba so thuc

---
+++ BAI9/bai9/bai9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace bai9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so thuc thu nhat: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Nhap so thuc thu hai: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Nhap so thuc thu ba: ");
+            double c = double.Parse(Console.ReadLine());
+            double max, min; //khai báo biến max và min để lưu giá trị lớn nhất và nhỏ nhất
+            FindMaxMin(a, b, c, out max, out min); //gọi phương thức FindMaxMin để tìm giá trị lớn nhất và nhỏ nhất
+            Console.WriteLine($"Gia tri lon nhat: {max}");
+            Console.WriteLine($"Gia tri nho nhat: {min}");
+        }
+        static void FindMaxMin(double x, double y, double z, out double max, out double min)
+        //phương thức FindMaxMin nhận vào 3 số thực và trả về giá trị lớn nhất và nhỏ nhất thông qua tham chiếu out
+        {
+            max = Math.Max(x, Math.Max(y, z));
+            min = Math.Min(x, Math.Min(y, z));
+        }
+    }
+}
