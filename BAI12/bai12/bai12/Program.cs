371d52c57865d2fcc8a9cbec837565811973c6ad
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:20:48 +0700

    Tuan 1/Bai 12/Xu ly chuoi: chuyen chu thuong, chu hoa va dem so tu

---
+++ BAI12/bai12/bai12/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+namespace bai12
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Nhap chuoi: ");
+            string text = Console.ReadLine();
+            string chuoiThuong = text.ToLower();// Chuyển sang chữ thường và chữ hoa
+            string chuoiHoa = text.ToUpper();
+            string[] danhSachTu = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);// Cắt chuỗi dựa trên khoảng trắng và loại bỏ các khoảng trắng thừa
+            int soTu = danhSachTu.Length;// Đếm số từ trong chuỗi
+            Console.WriteLine($"Chuoi chu thuong {chuoiThuong}");
+            Console.WriteLine($"Chuoi chu hoa:    {chuoiHoa}");
+            Console.WriteLine($"So tu trong chuoi: {soTu}");
+        }
+    }
+}
