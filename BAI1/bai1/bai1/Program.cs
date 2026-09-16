1ff45b7b79b28430c30713b1859df59b261f2163
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:10:01 +0700

    Tuan 1/Bai 1/Lam chuong trinh C# nhap va xuat ho ten, xem ma MSIL bang ildasm va chuyen MSIL ve file PE bang ilasm

---
+++ BAI1/bai1/bai1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string hoten = Console.ReadLine();
+
+            Console.WriteLine("Ho ten cua ban la: " + hoten);
+        }
+    }
+}
