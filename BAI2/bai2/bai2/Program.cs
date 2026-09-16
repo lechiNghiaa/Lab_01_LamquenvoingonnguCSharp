4e2d46d6b84d9dbd13bd335ef22f07bcd3f8627f
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:12:39 +0700

    Tuan 1/Bai 2/Lam chuong trinh nhap ho ten va xuat loi chao theo dinh dang yeu cau

---
+++ BAI2/bai2/bai2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string hoten = Console.ReadLine();
+            Console.WriteLine("Chao ban " + hoten + "!");
+        }
+    }
+}
