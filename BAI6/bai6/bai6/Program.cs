ce365b4ce8455330f6340c5cf9431676b93f324c
Chi Nghia <lechinghia1412@gmail.com>
2026-09-16 14:16:40 +0700

    Tuan 1/Bai 6/Xay dung phuong thuc tra ve gia tri lon nhat cua ba so nguyen

---
+++ BAI6/bai6/bai6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace bai6
+{
+    class Program
+    {
+        public static int max(int a, int b, int c)
+        {
+            return (a > b && a > c) ? a : (b > c) ? b : c;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen thu nhat: ");
+            int a = int.Parse(Console.ReadLine() ?? "0");
+            Console.Write("Nhap so ngyen thu hai: ");
+            int b = int.Parse(Console.ReadLine() ?? "0");
+            Console.Write("Nhap so nguyen thu ba: ");
+            int c = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine("Gia tri lon nhat la: {0}", max(a, b, c));
+        }
+    }
+}
